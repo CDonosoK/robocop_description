165a6a928846a3f40738979140cccb942c6f31a5
cdonosok <clemente.donosok@gmail.com>
2022-09-29 19:07:15 -0300

    Simple code to show camera

---
+++ codes/camera/camera.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+import time
+
+# Create a VideoCapture object
+cap = cv2.VideoCapture(2)
+
+# Check if camera opened successfully
+if (cap.isOpened()== False):
+    print("Error opening video stream or file")
+
+# Read until video is completed
+while(cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:
+        # Display the resulting frame
+        cv2.imshow('Frame',frame)
+
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # Break the loop
+    else:
+        break
+
+# When everything done, release the video capture object
+cap.release()
