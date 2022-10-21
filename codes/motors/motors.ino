ef7f35847cbca7b52f9e30d5107a71878c9c5773
cdonosok <clemente.donosok@gmail.com>
2022-10-21 01:54:56 -0300

    Upload new odometry information, still not working

--- codes/motors/motors.ino
+++ codes/motors/motors.ino
@@ -1,126 +1,239 @@
+// Se importan las librerías
+#include <ros.h>
 #include <AFMotor.h>
-AF_DCMotor Motor1(1);
-AF_DCMotor Motor2(2);
 
-// Variables from control
-String data, joystick_izq, joystick_der, buttons;
-int separ1, separ2, separ_joyIzq, separ_joyDer, separ_bot1, separ_bot2;
-float joy_izqX, joy_izqY, joy_derX, joy_derY;
-int bot1, bot2, bot3, bot4;
+// Se importan los mensajes
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <std_msgs/UInt16.h>
+#include <ros/time.h>
 
-float vel_forward, vel_backward, vel_turn;
+#define PI 3.1415926535897932384626433832795
 
-// Variables for motors
-int PWMA = 5, PWMB = 4, DA = 0, DB = 2;
+// Variables globales de ROS
+ros::NodeHandle nHandler;
+nav_msgs::Odometry odom_msgs;
+geometry_msgs::TransformStamped tOdom;
+geometry_msgs::TransformStamped tTF;
+tf::TransformBroadcaster broadcaster;
 
-// Variables for encoders
-int ENCR_1 = 8, ENCR_2 = 7;
-int DATA_ER1, DATA_ER2;
+// Se instancian las variables
+float linearVelocity;
+float angularVelocity;
+int forwardRight, forwardLeft, turnRight, turnLeft;
+AF_DCMotor Motor1(3);
+AF_DCMotor Motor2(4);
+
+const byte encoder_rightA = 18;
+const byte encoder_rightB = 19;
+const byte encoder_leftA = 20;
+const byte encoder_leftB = 21;
+
+static long counter_right = 0;
+static long counter_left = 0;
+static long counter_right_old = 0;
+static long counter_left_old = 0;
+
+volatile bool fired_right, fired_left;
+volatile bool up_right, up_left;
+
+double x = 0;
+double y = 0;
+double theta = 0;
+
 
 float map_float(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+void callback(const geometry_msgs::Twist& vel){
+  linearVelocity = vel.linear.x;
+  angularVelocity = vel.angular.z;
+
+  int right_velocity = 100;
+  int left_velocity = 100;
+  if (linearVelocity>0.1){
+    Motor1.run(FORWARD);
+    Motor2.run(FORWARD);
+    Motor1.setSpeed(right_velocity);
+    Motor2.setSpeed(left_velocity);
+  }
+  if (linearVelocity<-0.1){
+    Motor1.run(BACKWARD);
+    Motor2.run(BACKWARD);
+    Motor1.setSpeed(right_velocity);
+    Motor2.setSpeed(left_velocity);
+  }
+  if (angularVelocity>0.1){
+    Motor1.run(FORWARD);
+    Motor2.run(BACKWARD);
+    Motor1.setSpeed(right_velocity);
+    Motor2.setSpeed(left_velocity);
+  }
+  if (angularVelocity<-0.1){
+    Motor1.run(BACKWARD);
+    Motor2.run(FORWARD);
+    Motor1.setSpeed(right_velocity);
+    Motor2.setSpeed(left_velocity);
+  }
+  if (abs(angularVelocity)<0.1 and abs(linearVelocity)<0.1){
+    Motor1.setSpeed(0);
+    Motor2.setSpeed(0);
+  }
+}
+
+// Creación de los nodos de ROS
+ros::Subscriber<geometry_msgs::Twist> nodo_vel("cmd_vel", callback);
+
+// Variables para la odometría
+double odom_x = 1.0;
+double odom_y = 0.0;
+double odom_thet = 1.57;
+
+void read_right(){
+  if (digitalRead(encoder_rightA)){
+    up_right = !digitalRead(encoder_rightB);
+  }
+  else{
+    up_right = digitalRead(encoder_rightB);
+  }
+  fired_right = true;
+}
+void read_left(){
+  if (digitalRead(encoder_leftA)){
+    up_left = !digitalRead(encoder_leftB);
+  }
+  else{
+    up_left = digitalRead(encoder_leftB);
+  }
+  fired_left = true;
+}
 
 void setup(){
-  Motor1.setSpeed(200);    // Definimos la velocidad de  Motor1
+  // Se ajustan los nodos
+  nHandler.getHardware()->setBaud(115200);
+  nHandler.initNode();
+  nHandler.subscribe(nodo_vel);
+  broadcaster.init(nHandler);
+
+  // Se ajustan los motores
+  Motor1.setSpeed(100);
+  Motor2.setSpeed(100);
   Motor1.run(RELEASE);
-  Motor2.setSpeed(200);    // Definimos la velocidad de  Motor1
-  Motor2.run(RELEASE);  
+  Motor2.run(RELEASE);
+
+  // Se ajustan los encoders
+  pinMode(encoder_rightA, INPUT);
+  pinMode(encoder_rightB, INPUT);
+  pinMode(encoder_leftA, INPUT);
+  pinMode(encoder_leftB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(encoder_rightA), read_right, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder_leftA), read_left, CHANGE);
+
+  // Se ajusta la velocidad
+  int left_velocity = 100;
+  int right_velocity = 100;
 
   Serial.begin(115200);
-
-  // Setup encoder input
-  pinMode(ENCR_1, INPUT);
-  pinMode(ENCR_2, INPUT);
 }
 
-// Function to move motors 
-void motorControl(float joy_izqX, float joy_izqY, float joy_derX, float joy_derY){
-  // Stay
-  if (abs(joy_izqY) < 0.1 and abs(joy_derX) < 0.1){
-    Motor1.run(0);
-    Motor2.run(0);
-    Serial.println("Stay");
-  }
-  
-  // Forward
-  else if (joy_izqY > 0.1){
-    vel_forward = map_float(joy_izqY, 0.1, 1.00, 0, 255);
-    Motor1.run(FORWARD);
-    Motor2.run(FORWARD);
-    Serial.println("Forward");
-  }
-
-  // Backward
-  else if (joy_izqY < -0.1){
-    vel_backward = map_float(abs(joy_izqY), 0.1, 1.00, 0, 255);
-    Motor1.run(BACKWARD);
-    Motor2.run(BACKWARD);
-    Serial.println("Backward");
-  }
-
-  // Turn left on point
-  else if (abs(joy_izqY) <= 0.1 and joy_derX >= 0.1){
-    vel_turn = map_float(joy_derX, 0, 1.00, 0, 255);
-    Motor1.run(FORWARD);
-    Motor2.run(BACKWARD);
-    Serial.println("Turn left on point");
-  }
-
-  // Turn right on point
-  else if (abs(joy_izqY) <= 0.1 and joy_derX <= -0.1){
-    vel_turn = map_float(abs(joy_derX), 0.1, 1.00, 0, 255);
-    Motor1.run(BACKWARD);
-    Motor2.run(FORWARD);
-    Serial.println("Turn right on point");
-  }
-
-}
-
-void loop() {
-  if (Serial.available()){
-    // Obtain data from python
-    Serial.println("holiwi");
-    data = Serial.readStringUntil('\n');
-    Serial.println(data);
-    separ1 = data.indexOf("|");
-    separ2 = data.indexOf("|",separ1+1);
-
-    // Obtain data for left joystick
-    joystick_izq = data.substring(1, separ1-1);
-    separ_joyIzq = joystick_izq.indexOf(",");
-    joy_izqX = (joystick_izq.substring(0,separ_joyIzq)).toFloat();
-    joy_izqY = (joystick_izq.substring(separ_joyIzq+1, joystick_izq.length())).toFloat();
-
-    // Obtain data for right joystick
-    joystick_der = data.substring(separ1+2, separ2-1);
-    separ_joyDer = joystick_der.indexOf(",");
-    joy_derX = (joystick_der.substring(0,separ_joyDer)).toFloat();
-    joy_derY = (joystick_der.substring(separ_joyDer+1, joystick_der.length())).toFloat();
-
-    // Obtain data for buttons.
-    buttons = data.substring(separ2+2, data.length()-1);
-    separ_bot1 = buttons.indexOf(",");
-    bot1 = (buttons.substring(0,separ_bot1)).toInt();
-    separ_bot2 = buttons.indexOf(",", separ_bot1+1);
-    bot2 = (buttons.substring(separ_bot1+1,separ_bot2)).toInt();
-    separ_bot1 = buttons.indexOf(",", separ_bot2+1);
-    bot3 = (buttons.substring(separ_bot2+1,separ_bot1)).toInt();
-    separ_bot2 = buttons.indexOf(",", separ_bot1+1);
-    bot4 = (buttons.substring(separ_bot1+1,buttons.length())).toInt();
-
-    // Move motors
-    motorControl(joy_izqX, joy_izqY, joy_derX, joy_derY);
-  }
-  
-}
-
-// Function to read data from encoders
-void readEncoders(int DATA_ER1, int DATA_ER2){
-  Serial.print(DATA_ER1);
+void print_values(){
+  Serial.print(counter_right);  
   Serial.print(" - ");
-  Serial.println(DATA_ER2);
-
+  Serial.println(counter_left);
 }
 
+void update_counter(){
+  // Encoder derecho 
+  if (fired_right){
+    if (up_right){
+      counter_right++;      
+    }
+    else{
+      counter_right--;      
+    }
+    fired_right = false;
+  }
+
+  // Encoder izquierdo
+  if (fired_left){
+    if (up_left){
+      counter_left++;      
+    }
+    else{
+      counter_left--;      
+    }
+    fired_left = false;
+  }  
+}
+
+int rightTick[] = {
+  11154,
+  10868,
+  9931,
+  9835,
+  9581,
+  9944,
+  9000,
+  10265,  
+  9985,
+  9521
+};
+
+
+int leftTick[] = {
+  1140,
+  1134,
+  1146,
+  1132,
+  1142,
+  1123,
+  1138,
+  1130,
+  1121,
+  1111  
+};
+
+double mean(bool type){
+  double sum = 0;
+  double len = 0.0;
+  if (type){
+    len = sizeof(rightTick)/sizeof(rightTick[0]);
+    for (int i = 0; i < len; i++){
+      sum += rightTick[i];
+    }
+  }
+  else{
+    len = sizeof(leftTick)/sizeof(leftTick[0]);
+    for (int i = 0; i < len; i++){
+      sum += leftTick[i];
+    }
+  }
+  return sum / len;
+}
+
+void loop(){ 
+  update_counter();
+  double dx = 0.2;
+  double dtheta = 0.18;
+  x += cos(theta)*dx*0.1;
+  y += sin(theta)*dx*0.1;
+  theta += dtheta*0.1;
+  if (theta >PI){
+    theta =-PI;
+  }
+
+  double meanRight = mean(true);
+  double leftRight = mean(false);
+
+  tOdom.header.frame_id = "/odom";
+  tOdom.child_frame_id = "/base_link";
+  tOdom.transform.translation.x = 0.0;
+  tOdom.transform.translation.y = 0.0;
+  tOdom.transform.rotation = tf::createQuaternionFromYaw(0.0);
+  tOdom.header.stamp = nHandler.now();
+  broadcaster.sendTransform(tOdom); 
+  nHandler.spinOnce();
+}
